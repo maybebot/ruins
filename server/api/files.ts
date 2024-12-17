a4fd626a48ada51ff000ba3451a013334b5435eb
Ian Bobinac <ian@ianb.dev>
2024-12-17 22:01:11 +0100

    feat: add grouped options in files

--- server/api/files.ts
+++ server/api/files.ts
@@ -1,19 +1,27 @@
-import { getLintResults } from "#imports";
+import { getLintResults, getConfig } from "#imports";
 
+interface DataPoint {
+  name: string;
+  error: number;
+  warning: number;
+  total: number;
+  data?: DataPoint[];
+}
 interface FilesRes {
-  data: { name: string; error: number; warning: number; total: number }[];
+  data: DataPoint[];
 }
 
+/** sortBy: error | warning | total, grouped */
 export default eventHandler(async (event): Promise<FilesRes> => {
   const lint = await getLintResults();
+  const config = await getConfig();
 
-  let sortBy = new URLSearchParams(event.path.split("?")[1]).get("sortBy") as
-    | "error"
-    | "warning"
-    | "total";
+  const params = new URLSearchParams(event.path.split("?")[1]);
+  let sortBy = params.get("sortBy") as "error" | "warning" | "total";
   if (!["error", "warning", "total"].includes(sortBy)) {
     sortBy = "total";
   }
+  const isGrouped = Boolean(params.get("grouped"));
 
   const data = lint.issues
     .map((issue) => ({
@@ -23,6 +31,24 @@ export default eventHandler(async (event): Promise<FilesRes> => {
       total: issue.errorCount + issue.warningCount,
     }))
     .sort((a, b) => b[sortBy] - a[sortBy]);
+
+  if (isGrouped && config?.group?.dirs) {
+    const grouped = config.group.dirs.reduce((acc: DataPoint[], dir) => {
+      const files = data.filter((f) => f.name.startsWith(dir));
+      if (files.length > 0) {
+        acc.push({
+          name: dir,
+          error: files.reduce((acc, f) => acc + f.error, 0),
+          warning: files.reduce((acc, f) => acc + f.warning, 0),
+          total: files.reduce((acc, f) => acc + f.total, 0),
+          data: files,
+        });
+      }
+      return acc;
+    }, [] as typeof data);
+
+    return { data: grouped };
+  }
 
   return { data };
 });
