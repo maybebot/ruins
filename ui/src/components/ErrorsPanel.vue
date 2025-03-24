930fbab247f07ab16f63aaff7c36f9c3e5137bb6
Ian Bobinac <ian@ianb.dev>
2025-03-24 22:16:49 +0100

    feat(ui): make minor tweaks

--- ui/src/components/ErrorsPanel.vue
+++ ui/src/components/ErrorsPanel.vue
@@ -4,10 +4,12 @@
 
         <pyro-box>
             <FilterBar @input="setFilter" />
-            <pyro-scrollbox style="height: 100%">
+            <pyro-scrollbox style="height: 100%; max-height: 25vh">
                 <EmptyState v-if="noData" />
                 <div v-else v-for="error in filteredErrors" :key="error.id" class="errors-table">
-                    <div>{{ error.name }}</div>
+                    <div style="display: flex; gap: 0.5em">
+                        <component :is="getCountSvg(error.total)" style="opacity: 0.3" />{{ error.name }}
+                    </div>
                     <div>{{ error.total }}</div>
                 </div>
             </pyro-scrollbox>
@@ -19,6 +21,11 @@
 import { computed, ref } from 'vue';
 import FilterBar from './FilterBar.vue';
 import EmptyState from './EmptyState.vue';
+import Count1 from '@/assets/count/1.svg';
+import Count2 from '@/assets/count/2.svg';
+import Count3 from '@/assets/count/3.svg';
+import Count4 from '@/assets/count/4.svg';
+import Count5 from '@/assets/count/5.svg';
 import 'pyro/scrollbox';
 
 const props = defineProps<{
@@ -42,6 +49,28 @@ const filteredErrors = computed(() => {
     });
 });
 
+const countMap = {
+    1: Count1,
+    2: Count2,
+    3: Count3,
+    4: Count4,
+    5: Count5,
+}
+const getCountSvg = (count) => {
+    switch (count) {
+        case 1:
+            return Count1;
+        case 2:
+            return Count2;
+        case 3:
+            return Count3;
+        case 4:
+            return Count4;
+        default:
+            return Count5;
+
+    }
+}
 
 </script>
 
@@ -56,6 +85,7 @@ const filteredErrors = computed(() => {
     display: flex;
     justify-content: space-between;
     border-bottom: 1px solid #e5e7eb44;
+    padding: 0 var(--pyro-spacing);
 }
 
 pyro-box {
