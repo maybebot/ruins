c20a6a30c0ca02563c582170e4e029948e1cd374
Ian Bobinac <ian@ianb.dev>
2025-04-18 08:51:36 +0200

    fix(cli): reset ignores before relinting

--- cli/create-lint-file.ts
+++ cli/create-lint-file.ts
@@ -1,4 +1,5 @@
 import { execSync } from "child_process";
+import { writeFile } from "node:fs/promises";
 import { getConfig } from "../config/getConfig.js";
 import { resolve } from "node:path";
 import consola from "consola";
@@ -12,15 +13,18 @@ import consola from "consola";
 export const createLintFile = async (ruinsPath: string, binPath: string) => {
   const settings = await getConfig();
   const outputFile = resolve(settings.dir, settings.files.eslintIssues);
+  const ignoresFile = resolve(settings.dir, settings.files.eslintIgnores);
   consola.info(`Searching for linting issues`);
+
+  // reset to avoid skipping already ignored issues
+  const resetConent = `export const ruinsIgnores = [];`;
+  await writeFile(ignoresFile, resetConent);
 
   try {
     await execSync(
       `${binPath}/eslint --quiet -o ${outputFile} -f ${ruinsPath}/dist/cli/formatters/output.js`
     );
-    consola.success(
-      `Collected issues in ${settings.dir}${settings.files.eslintIssues}`
-    );
+    consola.success(`Collected issues in ${settings.dir}${settings.files.eslintIssues}`);
   } catch {
     // It always has a non-zero exit code
   }
