8d1bf38db6146bfb638b6cc0fd7ee8609f4d529b
Ian Bobinac <ian@ianb.dev>
2025-11-09 22:06:27 +0100

    fix(config): simplify imports

--- modules/config/index.ts
+++ modules/config/index.ts
@@ -1,15 +1,19 @@
-import { RuinsModule } from "@ruins/types";
-import type { RuinsConfig } from "./types";
 import { loadConfig } from "c12";
+import { RuinsModule } from "../types";
 
-export { RuinsConfig } from "./types";
-
-export const ruinsModule: RuinsModule = {
-  config: {
-    name: "config",
-    description: "[internal] module for loading ruins.config",
-  },
-};
+/**
+ * Definition of Ruins config in user's /ruins.config.ts file
+ */
+export interface RuinsConfig {
+  /** Directory where ruins files are stored. Do not .gitignore */
+  dir: string;
+  /** Paths in your app to directories you want to show grouped results */
+  group?: {
+    dirs: string[];
+  };
+  /** Module-specific configuration */
+  modules: Promise<RuinsModule>[];
+}
 
 /** Returns user configuration in ruins.config.ts */
 export const readConfig = async () => {
